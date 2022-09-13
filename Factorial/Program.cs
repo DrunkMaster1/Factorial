3658193b138b2de6dc8c9d6552693e9f7d6df425
Глеб Бутяев <iiaiias1992@gmail.com>
2022-09-13 18:26:10 +0300

    Решил факториал

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using System.Numerics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите число факториала:");
+        int Factorial1 = int.Parse(Console.ReadLine());
+        Console.WriteLine(fact(Factorial1));
+        Console.ReadLine();
+    }
+    static int fact(int num)
+    {
+        return (num == 0) ? 1 : num * fact(num - 1);
     }
 }
